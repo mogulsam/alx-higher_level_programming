36a053cc6e35830ab63e40e1b0df5eb78bc9cba1
mogulsam <theejaguarscomedy@gmail.com>
2024-01-04 01:36:23 -0800

    py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+First_3 =word [:3]
+Last_2 =word[-2:]
+Middle_word =word [1:-1]
+print("Firt 3 letters: {}".format(First_3))
+print("Last 2 letters: {}".format(Last_2))
+print("Middle word: {}".format(Middle_word))
