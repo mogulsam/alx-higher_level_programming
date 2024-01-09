1d134477524d1c795b6c10d41ff89f1e892d3eae
mogulsam <theejaguarscomedy@gmail.com>
2024-01-09 04:46:49 -0800

    data structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-   if idx < 0 or idx > len(my_list)-1:
-      copy= my_list.copy()
-      return my_list.copy()
-   else:
-      copy[idx]=element
-      return copy
-   
-def no_c(my_string):
-   new_string= my_string.translate({ord(i): None for i in'cC'})
-   print(new_string)
+    copy = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list.copy()
+    else:
+        copy[idx] = element
+        return copy
