868ab38bca94a259db4e552baf9dd1765ee99b34
mogulsam <theejaguarscomedy@gmail.com>
2024-03-14 04:25:54 -0700

    final updates after checking on a few errors

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,3 @@
---a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 -- Lists all records of the table second_table having a name value in my MySQL server.
 -- Records are ordered by descending score.
 SELECT `score`, `name`
